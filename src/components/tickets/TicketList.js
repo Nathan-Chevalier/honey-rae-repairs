33044a7cc495201474b181c3780653358af70e3e
Nathan Chevalier <cerberusgfx@gmail.com>
2023-08-31 15:29:20 -0500

    Search bar functional

--- src/components/tickets/TicketList.js
+++ src/components/tickets/TicketList.js
@@ -2,11 +2,13 @@ import { useEffect, useState } from "react";
 import { getAllTickets } from "../../services/ticketServices";
 import { Ticket } from "./Ticket";
 import "./Tickets.css";
+import { FilterBar } from "./FilterBar";
 
 export const TicketList = () => {
   const [allTickets, setAllTickets] = useState([]);
   const [showEmergencyOnly, setShowEmergency] = useState(false);
   const [filteredTickets, setFilteredTickets] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     getAllTickets().then((ticketsArray) => {
@@ -26,32 +28,23 @@ export const TicketList = () => {
     }
   }, [showEmergencyOnly, allTickets]);
 
+  useEffect(() => {
+    const foundTickets = allTickets.filter((ticket) =>
+      ticket.description.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredTickets(foundTickets);
+  }, [searchTerm, allTickets]);
+
   return (
     <div className="tickets-container">
       <h2>Tickets</h2>
-      <div>
-        <button
-          className="filter-btn btn-primary"
-          onClick={() => {
-            setShowEmergency(true);
-          }}
-        >
-          Emergency
-        </button>
-        <div>
-          <button
-            className="filter-btn btn-info"
-            onClick={() => {
-              setShowEmergency(false);
-            }}
-          >
-            Show All
-          </button>
-        </div>
-      </div>
+      <FilterBar
+        setShowEmergency={setShowEmergency}
+        setSearchTerm={setSearchTerm}
+      />
       <article className="tickets">
         {filteredTickets.map((ticket) => {
-          return <Ticket ticket={ticket} key={ticket.id} name={"Joe"} />;
+          return <Ticket ticket={ticket} key={ticket.id} />;
         })}
       </article>
     </div>
