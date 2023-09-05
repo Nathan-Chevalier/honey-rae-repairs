08bf3ea9a971951f073122d8da08e69ae656249e
Nathan Chevalier <cerberusgfx@gmail.com>
2023-09-05 15:57:12 -0500

    Routing functional

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import "./App.css";
 import { CustomerList } from "./components/customers/CustomersList";
 import { EmployeesList } from "./components/employees/EmployeesList";
+import { NavBar } from "./components/nav/NavBar";
 import { TicketList } from "./components/tickets/TicketList";
+import { Routes, Route, Outlet } from "react-router-dom";
 
 export const App = () => {
   return (
-    <>
-      {/* <TicketList /> */}
-      {/* <CustomerList /> */}
-      <EmployeesList />
-    </>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <NavBar />
+            <Outlet />
+          </>
+        }
+      >
+        <Route path="tickets" element={<TicketList />} />
+        <Route path="customers" element={<CustomerList />} />
+        <Route path="employees" element={<EmployeesList />} />
+      </Route>
+    </Routes>
   );
 };
