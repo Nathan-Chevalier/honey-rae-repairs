d2580cd776c85c4ab6ac8a80d01f20406db956da
Nathan Chevalier <cerberusgfx@gmail.com>
2023-09-06 11:09:05 -0500

    useParams implemented in routing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { EmployeesList } from "./components/employees/EmployeesList";
 import { NavBar } from "./components/nav/NavBar";
 import { TicketList } from "./components/tickets/TicketList";
 import { Routes, Route, Outlet } from "react-router-dom";
+import { Welcome } from "./components/welcome/Welcome";
+import { CustomerDetails } from "./components/customers/CustomerDetails";
 
 export const App = () => {
   return (
@@ -17,9 +19,13 @@ export const App = () => {
           </>
         }
       >
+        <Route index element={<Welcome />} />
         <Route path="tickets" element={<TicketList />} />
-        <Route path="customers" element={<CustomerList />} />
         <Route path="employees" element={<EmployeesList />} />
+        <Route path="customers">
+          <Route index element={<CustomerList />} />
+          <Route path=":customerId" element={<CustomerDetails />} />
+        </Route>
       </Route>
     </Routes>
   );
